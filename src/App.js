778cc56b8ef10d8c96069ae4e49bd6e5ccb34fd4
Raphael <raphael@raphael.WLAN_AP>
2021-07-07 22:40:15 -0300

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,20 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-// import Repeticao from './Repeticao';
-// import Condicional from './Condicional';
-import Produtos from './Produtos';
 
+class App extends Component {
 
-function App() {
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-       {/* <Repeticao></Repeticao> */}
-       <Produtos  numero={10} />
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'React CRUD',
+      act: 0,
+      index: '',
+      datas: []
+    }
+  } 
+
+  componentDidMount(){
+    this.refs.name.focus();
+  }
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+    console.log('try');
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
+
+    if(this.state.act === 0){   //novo
+      let data = {
+        name, address
+      }
+      datas.push(data);
+    }else{                      //update
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].address = address;
+    }    
+
+    this.setState({
+      datas: datas,
+      act: 0
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fEdit = (i) => {
+    let data = this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.address.value = data.address;
+
+    this.setState({
+      act: 1,
+      index: i
+    });
+
+    this.refs.name.focus();
+  }  
+
+  render() {
+    let datas = this.state.datas;
+    return (
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="name" placeholder="Nome" className="formField" />
+          <input type="text" ref="address" placeholder="Endereço" className="formField" />
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">Cadastrar</button>
+        </form>
+        <pre>
+          {datas.map((data, i) =>
+            <li key={i} className="myList">
+              {i+1}. {data.name}, {data.address}
+              <button onClick={()=>this.fRemove(i)} className="myListButton">remover</button>
+              <button onClick={()=>this.fEdit(i)} className="myListButton">editar</button>
+            </li>
+          )}
+        </pre>
+      </div>
+    );
+  }
 }
 
 export default App;
